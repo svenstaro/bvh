5c6f1722f09b2ecea4581dc5eb55032b52ca7401
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-09-28 15:03:33 +0200

    Add documentation, add benchmarks

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,10 +1,23 @@
+//! This module defines a [`BVH`] building procedure as well as a [`BVH`] flattening procedure
+//! so that the recursive structure can be easily used in compute shaders.
+//!
+//! [`BVH`]: struct.BVH.html
+//!
+
 use aabb::{AABB, Bounded};
 use ray::Ray;
 use std::boxed::Box;
 use std::f32;
 use std::iter::repeat;
-// TODO kommentar zu wieso keine root aabb
-/// Enum which describes the union type of `BVHNode`s.
+
+/// Enum which describes the union type of a node in a [`BVH`]s.
+/// This structure does not allow to store a root node's [`AABB`]. Therefore rays
+/// which do not hit the root [`AABB`] perform two [`AABB`] tests (left/right) instead of one.
+/// On the other hand this structure decreases the total number of indirections.
+///
+/// [`AABB`]: ../aabb/struct.AABB.html
+/// [`BVH`]: struct.BVH.html
+///
 enum BVHNode {
     /// Leaf node.
     Leaf {
@@ -22,15 +35,45 @@ enum BVHNode {
     },
 }
 
+/// A structure of a node of a flat [`BVH`]. The structure of the nodes allows for an
+/// iterative traversal approach without the necessity to maintain a stack or queue.
+///
+/// [`BVH`]: struct.BVH.html
+///
 pub struct FlatNode {
+    /// The [`AABB`] of the [`BVH`] node. Prior to testing the [`AABB`] bounds, the `entry_index`
+    /// must be checked. In case the entry_index is [`u32::max_value()`], the [`AABB`] is meaningless.
+    ///
+    /// [`AABB`]: ../aabb/struct.AABB.html
+    /// [`BVH`]: struct.BVH.html
+    /// [`u32::max_value()`]: https://doc.rust-lang.org/std/u32/constant.MAX.html
+    ///
     aabb: AABB,
+
+    /// The index of the `FlatNode` to jump to, if the [`AABB`] test is positive.
+    /// If this value is [`u32::max_value()`] then the current node is a leaf node.
+    /// Leaf nodes contain a shape index and an exit index. In leaf nodes the
+    /// [`AABB`] is undefined.
+    ///
+    /// [`AABB`]: ../aabb/struct.AABB.html
+    /// [`u32::max_value()`]: https://doc.rust-lang.org/std/u32/constant.MAX.html
+    ///
     entry_index: u32,
+
+    /// The index of the `FlatNode` to jump to, if the [`AABB`] test is negative.
+    ///
+    /// [`AABB`]: ../aabb/struct.AABB.html
+    ///
     exit_index: u32,
+
+    /// The index of the shape in the shapes array.
     shape_index: u32,
 }
 
-const MAX_UINT32: u32 = 0xFFFFFFFF;
-
+/// Prints a textual representation of a flat [`BVH`].
+///
+/// [`BVH`]: struct.BVH.html
+///
 pub fn print_flat_tree(flat_nodes: &[FlatNode]) {
     for (i, node) in flat_nodes.iter().enumerate() {
         println!("{}\tentry {}\texit {}\tshape {}",
@@ -41,22 +84,21 @@ pub fn print_flat_tree(flat_nodes: &[FlatNode]) {
     }
 }
 
-pub fn traverse_flat_bvh<'a, T: Bounded>(ray: &Ray,
-                                         flat_nodes: &[FlatNode],
-                                         shapes: &'a [T])
-                                         -> Vec<&'a T> {
+/// Traverses a flat [`BVH`] structure.
+/// Returns a [`Vec`] of indices which are hit with a high probability.
+///
+/// [`Vec`]: https://doc.rust-lang.org/std/vec/struct.Vec.html
+/// [`BVH`]: struct.BVH.html
+///
+pub fn traverse_flat_bvh(ray: &Ray, flat_nodes: &[FlatNode]) -> Vec<usize> {
     let mut hit_shapes = Vec::new();
     let mut index = 0;
     let max_length = flat_nodes.len();
     while index < max_length {
         let node = &flat_nodes[index];
-        if node.entry_index == MAX_UINT32 {
+        if node.entry_index == u32::max_value() {
             let shape_index = node.shape_index;
-            let actual_shape = &shapes[shape_index as usize];
-            let actual_aabb = actual_shape.aabb();
-            if ray.intersects_aabb(&actual_aabb) {
-                hit_shapes.push(actual_shape);
-            }
+            hit_shapes.push(shape_index as usize);
             index = node.exit_index as usize;
         } else if ray.intersects_aabb(&node.aabb) {
             index = node.entry_index as usize;
@@ -68,7 +110,11 @@ pub fn traverse_flat_bvh<'a, T: Bounded>(ray: &Ray,
 }
 
 impl BVHNode {
-    pub fn new<T: Bounded>(shapes: &[T], indices: Vec<usize>) -> BVHNode {
+    /// Builds a [`BVHNode`] recursively using SAH partitioning.
+    ///
+    /// [`BVHNode`]: enum.BVHNode.html
+    ///
+    pub fn build<T: Bounded>(shapes: &[T], indices: Vec<usize>) -> BVHNode {
 
         // Helper function to accumulate the AABB union and the centroids AABB.
         fn grow_union_bounds(union_bounds: (AABB, AABB), shape_aabb: &AABB) -> (AABB, AABB) {
@@ -187,12 +233,16 @@ impl BVHNode {
         // Construct the actual data structure
         BVHNode::Node {
             child_l_aabb: child_l_aabb,
-            child_l: Box::new(BVHNode::new(shapes, child_l_indices)),
+            child_l: Box::new(BVHNode::build(shapes, child_l_indices)),
             child_r_aabb: child_r_aabb,
-            child_r: Box::new(BVHNode::new(shapes, child_r_indices)),
+            child_r: Box::new(BVHNode::build(shapes, child_r_indices)),
         }
     }
 
+    /// Prints a textual representation of the recursive [`BVH`] structure.
+    ///
+    /// [`BVH`]: struct.BVH.html
+    ///
     fn print(&self, depth: usize) {
         let padding: String = repeat(" ").take(depth).collect();
         match *self {
@@ -208,6 +258,12 @@ impl BVHNode {
         }
     }
 
+    /// Traverses the [`BVH`] recursively and returns a [`Vec`]
+    /// of all shapes which are hit with a high probability.
+    ///
+    /// [`BVH`]: struct.BVH.html
+    /// [`Vec`]: https://doc.rust-lang.org/std/vec/struct.Vec.html
+    ///
     pub fn traverse_recursive(&self, ray: &Ray, indices: &mut Vec<usize>) {
         match *self {
             BVHNode::Node { ref child_l_aabb, ref child_l, ref child_r_aabb, ref child_r } => {
@@ -226,6 +282,12 @@ impl BVHNode {
         }
     }
 
+    /// Flattens the [`BVH`], so that it can be traversed in an iterative manner.
+    /// The iterative traverse procedure is implemented in [`traverse_flat_bvh`].
+    ///
+    /// [`BVH`]: struct.BVH.html
+    /// [`traverse_flat_bvh`]: method.traverse_flat_bvh.html
+    ///
     pub fn flatten_tree(&self, vec: &mut Vec<FlatNode>, next_free: usize) -> usize {
         match *self {
             BVHNode::Node { ref child_l_aabb, ref child_l, ref child_r_aabb, ref child_r } => {
@@ -234,7 +296,7 @@ impl BVHNode {
                     aabb: *child_l_aabb,
                     entry_index: (next_free + 1) as u32,
                     exit_index: 0,
-                    shape_index: MAX_UINT32,
+                    shape_index: u32::max_value(),
                 };
                 vec.push(child_l_node);
 
@@ -245,7 +307,7 @@ impl BVHNode {
                     aabb: *child_r_aabb,
                     entry_index: (index_after_child_l + 1) as u32,
                     exit_index: 0,
-                    shape_index: MAX_UINT32,
+                    shape_index: u32::max_value(),
                 };
                 vec.push(exit_node);
 
@@ -260,7 +322,7 @@ impl BVHNode {
                     next_shape += 1;
                     let leaf_node = FlatNode {
                         aabb: AABB::empty(),
-                        entry_index: MAX_UINT32,
+                        entry_index: u32::max_value(),
                         exit_index: next_shape as u32,
                         shape_index: *shape_index as u32,
                     };
@@ -273,27 +335,44 @@ impl BVHNode {
     }
 }
 
-/// The BVH data structure. Only contains the BVH structure and indices to
-/// the slice of shapes given in the `new` function.
+/// The [`BVH`] data structure. Only contains the [`BVH`] structure and indices to
+/// the slice of shapes given in the [`build`] function.
+///
+/// [`BVH`]: struct.BVH.html
+/// [`build`]: struct.BVH.html#method.build
+///
 pub struct BVH {
-    /// The root node of the BVH
+    /// The root node of the [`BVH`].
+    ///
+    /// [`BVH`]: struct.BVH.html
+    ///
     root: BVHNode,
 }
 
 impl BVH {
-    /// Creates a new BVH from the slice of shapes.
-    pub fn new<T: Bounded>(shapes: &[T]) -> BVH {
+    /// Creates a new [`BVH`] from the slice of shapes.
+    ///
+    /// [`BVH`]: struct.BVH.html
+    ///
+    pub fn build<T: Bounded>(shapes: &[T]) -> BVH {
         let indices = (0..shapes.len()).collect::<Vec<usize>>();
-        let root = BVHNode::new(shapes, indices);
+        let root = BVHNode::build(shapes, indices);
         BVH { root: root }
     }
 
-    /// Prints the BVH in a tree-like visualization.
+    /// Prints the [`BVH`] in a tree-like visualization.
+    ///
+    /// [`BVH`]: struct.BVH.html
+    ///
     pub fn print(&self) {
         self.root.print(0);
     }
 
-    /// Traverses the tree recursively. Returns an array of all shapes which were hit.
+    /// Traverses the tree recursively. Returns an array of all shapes, the [`AABB`]s of which
+    /// were hit.
+    ///
+    /// [`AABB`]: ../aabb/struct.AABB.html
+    ///
     pub fn traverse_recursive<'a, T: Bounded>(&'a self, ray: &Ray, shapes: &'a [T]) -> Vec<&T> {
         let mut indices = Vec::new();
         self.root.traverse_recursive(ray, &mut indices);
@@ -307,7 +386,10 @@ impl BVH {
         hit_shapes
     }
 
-    /// Flattens the BVH so that it can be traversed iteratively.
+    /// Flattens the [`BVH`] so that it can be traversed iteratively.
+    ///
+    /// [`BVH`]: struct.BVH.html
+    ///
     pub fn flatten_tree(&self) -> Vec<FlatNode> {
         let mut vec = Vec::new();
         self.root.flatten_tree(&mut vec, 0);
@@ -318,7 +400,7 @@ impl BVH {
 #[cfg(test)]
 mod tests {
     use aabb::{AABB, Bounded};
-    use bvh::{BVH, traverse_flat_bvh, print_flat_tree};
+    use bvh::{BVH, traverse_flat_bvh, print_flat_tree, FlatNode};
     use nalgebra::{Point3, Vector3};
     use std::collections::HashSet;
     use ray::Ray;
@@ -345,7 +427,7 @@ mod tests {
             shapes.push(XBox { x: x });
         }
 
-        let bvh = BVH::new(&shapes);
+        let bvh = BVH::build(&shapes);
         (shapes, bvh)
     }
 
@@ -418,47 +500,40 @@ mod tests {
         let flat_bvh = bvh.flatten_tree();
         print_flat_tree(&flat_bvh);
 
+        fn test_ray(ray: &Ray, flat_bvh: &[FlatNode], shapes: &[XBox]) -> Vec<usize> {
+            let hit_shapes = traverse_flat_bvh(ray, flat_bvh);
+            for (index, shape) in shapes.iter().enumerate() {
+                if !hit_shapes.contains(&index) {
+                    assert!(!ray.intersects_aabb(&shape.aabb()));
+                }
+            }
+            hit_shapes
+        }
+
         // Define a ray which traverses the x-axis from afar
         let position_1 = Point3::new(-1000.0, 0.0, 0.0);
         let direction_1 = Vector3::new(1.0, 0.0, 0.0);
         let ray_1 = Ray::new(position_1, direction_1);
-
-        // It shuold hit all shapes
-        let hit_shapes_1 = traverse_flat_bvh(&ray_1, &flat_bvh, &shapes);
-        assert!(hit_shapes_1.len() == 21);
-        let mut xs_1 = HashSet::new();
-        for shape in &hit_shapes_1 {
-            xs_1.insert(shape.x);
-        }
-        for x in -10..11 {
-            assert!(xs_1.contains(&x));
+        let hit_shapes_1 = test_ray(&ray_1, &flat_bvh, &shapes);
+        for i in 0..21 {
+            assert!(hit_shapes_1.contains(&i));
         }
 
         // Define a ray which traverses the y-axis from afar
         let position_2 = Point3::new(1.0, -1000.0, 0.0);
         let direction_2 = Vector3::new(0.0, 1.0, 0.0);
         let ray_2 = Ray::new(position_2, direction_2);
-
-        // It should hit only one box
-        let hit_shapes_2 = traverse_flat_bvh(&ray_2, &flat_bvh, &shapes);
-        assert!(hit_shapes_2.len() == 1);
-        assert!(hit_shapes_2[0].x == 1);
+        let hit_shapes_2 = test_ray(&ray_2, &flat_bvh, &shapes);
+        assert!(hit_shapes_2.contains(&11));
 
         // Define a ray which intersects the x-axis diagonally
         let position_3 = Point3::new(6.0, 0.5, 0.0);
         let direction_3 = Vector3::new(-2.0, -1.0, 0.0);
         let ray_3 = Ray::new(position_3, direction_3);
-
-        // It should hit exactly three boxes
-        let hit_shapes_3 = traverse_flat_bvh(&ray_3, &flat_bvh, &shapes);
-        assert!(hit_shapes_3.len() == 3);
-        let mut xs_3 = HashSet::new();
-        for shape in &hit_shapes_3 {
-            xs_3.insert(shape.x);
-        }
-        assert!(xs_3.contains(&6));
-        assert!(xs_3.contains(&5));
-        assert!(xs_3.contains(&4));
+        let hit_shapes_3 = test_ray(&ray_3, &flat_bvh, &shapes);
+        assert!(hit_shapes_3.contains(&15));
+        assert!(hit_shapes_3.contains(&16));
+        assert!(hit_shapes_3.contains(&17));
     }
 
     struct Triangle {
@@ -546,13 +621,19 @@ mod tests {
         vec
     }
 
+    fn create_ray(seed: &mut u64) -> Ray {
+        let origin = next_point3(seed);
+        let direction = next_point3(seed).to_vector();
+        Ray::new(origin, direction)
+    }
+
     #[bench]
     /// Benchmark the construction of a BVH with 120,000 triangles.
     fn bench_build_120k_triangles_bvh(b: &mut ::test::Bencher) {
         let triangles = create_n_cubes(10_000);
 
         b.iter(|| {
-            BVH::new(&triangles);
+            BVH::build(&triangles);
         });
     }
 
@@ -560,10 +641,87 @@ mod tests {
     /// Benchmark the flattening of a BVH with 120,000 triangles.
     fn bench_flatten_120k_triangles_bvh(b: &mut ::test::Bencher) {
         let triangles = create_n_cubes(10_000);
-        let bvh = BVH::new(&triangles);
+        let bvh = BVH::build(&triangles);
 
         b.iter(|| {
             bvh.flatten_tree();
+        });
+    }
+
+    #[bench]
+    /// Benchmark intersecting 120,000 triangles directly.
+    fn bench_intersect_120k_triangles_list(b: &mut ::test::Bencher) {
+        let triangles = create_n_cubes(10_000);
+        let mut seed = 0;
+
+        b.iter(|| {
+            let ray = create_ray(&mut seed);
+
+            // Iterate over the list of triangles
+            for triangle in &triangles {
+                ray.intersects_triangle(&triangle.a, &triangle.b, &triangle.c);
+            }
+        });
+    }
+
+    #[bench]
+    /// Benchmark intersecting 120,000 triangles with preceeding `AABB` tests.
+    fn bench_intersect_120k_triangles_list_aabb(b: &mut ::test::Bencher) {
+        let triangles = create_n_cubes(10_000);
+        let mut seed = 0;
+
+        b.iter(|| {
+            let ray = create_ray(&mut seed);
+
+            // Iterate over the list of triangles
+            for triangle in &triangles {
+                // First test whether the ray intersects the AABB of the triangle
+                if ray.intersects_aabb(&triangle.aabb()) {
+                    ray.intersects_triangle(&triangle.a, &triangle.b, &triangle.c);
+                }
+            }
+        });
+    }
+
+    #[bench]
+    /// Benchmark intersecting 120,000 triangles using the recursive BVH.
+    fn bench_intersect_120k_triangles_bvh_recursive(b: &mut ::test::Bencher) {
+        let triangles = create_n_cubes(10_000);
+        let bvh = BVH::build(&triangles);
+        let mut seed = 0;
+
+        b.iter(|| {
+            let ray = create_ray(&mut seed);
+
+            // Traverse the BVH recursively
+            let hits = bvh.traverse_recursive(&ray, &triangles);
+
+            // Traverse the resulting list of positive AABB tests
+            for triangle in &hits {
+                ray.intersects_triangle(&triangle.a, &triangle.b, &triangle.c);
+            }
+        });
+    }
+
+    #[bench]
+    /// Benchmark intersecting 120,000 triangles using the recursive BVH.
+    fn bench_intersect_120k_triangles_bvh_flat(b: &mut ::test::Bencher) {
+        let triangles = create_n_cubes(10_000);
+        let bvh = BVH::build(&triangles);
+        let flat_bvh = bvh.flatten_tree();
+        let mut seed = 0;
+
+        b.iter(|| {
+            let ray = create_ray(&mut seed);
+
+            // Traverse the flat BVH
+            let hits = traverse_flat_bvh(&ray, &flat_bvh);
+
+            // Traverse the resulting list of positive AABB tests
+            for index in &hits {
+                let triangle = &triangles[*index];
+                ray.intersects_triangle(&triangle.a, &triangle.b, &triangle.c);
+            }
         });
     }
 }
