5e7997463722a9eb9d62e4780c169e87a8f00727
Finn Bear <finnbearlabs@gmail.com>
2025-01-05 22:04:33 -0800

    Untested fixes.

--- src/bvh/bvh_impl.rs
+++ src/bvh/bvh_impl.rs
@@ -293,6 +293,11 @@ impl<T: BHValue, const D: usize> Bvh<T, D> {
     /// Checks if all children of a node have the correct parent index, and that there is no
     /// detached subtree. Also checks if the `Aabb` hierarchy is consistent.
     pub fn is_consistent<Shape: BHShape<T, D>>(&self, shapes: &[Shape]) -> bool {
+        if self.nodes.is_empty() {
+            // There is no node_index=0.
+            return vec;
+        }
+
         // The root node of the bvh is not bounded by anything.
         let space = Aabb::infinite();
 
@@ -380,6 +385,11 @@ impl<T: BHValue, const D: usize> Bvh<T, D> {
     where
         T: std::fmt::Display,
     {
+        if self.nodes.is_empty() {
+            // There is no node_index=0.
+            return vec;
+        }
+
         // The root node of the bvh is not bounded by anything.
         let space = Aabb::infinite();
 
@@ -414,6 +424,10 @@ impl<T: BHValue, const D: usize> Bvh<T, D> {
     /// Check that the `Aabb`s in the `Bvh` are tight, which means, that parent `Aabb`s are not
     /// larger than they should be.
     pub fn assert_tight(&self) {
+        if self.nodes.is_empty() {
+            // There is no node_index=0.
+            return vec;
+        }
         // When starting to check whether the `Bvh` is tight, we cannot provide a minimum
         // outer `Aabb`, therefore we compute the correct one in this instance.
         if let BvhNode::Node {
--- src/flat_bvh.rs
+++ src/flat_bvh.rs
@@ -232,6 +232,10 @@ impl<T: BHValue, const D: usize> Bvh<T, D> {
         F: Fn(&Aabb<T, D>, u32, u32, u32) -> FNodeType,
     {
         let mut vec = Vec::new();
+        if self.nodes.is_empty() {
+            // There is no node_index=0.
+            return vec;
+        }
         self.nodes[0].flatten_custom(&self.nodes, &mut vec, 0, constructor);
         vec
     }
@@ -379,7 +383,7 @@ impl<T: BHValue + std::fmt::Display, const D: usize> BoundingHierarchy<T, D> for
     /// let flat_bvh = FlatBvh::build(&mut shapes);
     /// let hit_shapes = flat_bvh.traverse(&ray, &shapes);
     /// ```
-    fn traverse<'a, B: Bounded<T, D>>(&'a self, ray: &Ray<T, D>, shapes: &'a [B]) -> Vec<&'a B> {
+    fn traverse<'a, B: Bounded<T, D>>(&'a self, ray: &Ray<T, D>, shapes: &'a [B]) -> Vec<&B> {
         let mut hit_shapes = Vec::new();
         let mut index = 0;
 
