3de2ee1a0ad29573233e7967c209ca7b7a8f6a3d
dashedman <assarbsmail@gmail.com>
2025-01-14 23:01:09 +0300

    Fix order of distance traversal, preserving old algorithm under a different name

--- src/bvh/mod.rs
+++ src/bvh/mod.rs
@@ -5,11 +5,13 @@
 
 mod bvh_impl;
 mod bvh_node;
+mod child_distance_traverse;
 mod distance_traverse;
 mod iter;
 mod optimization;
 
 pub use self::bvh_impl::*;
 pub use self::bvh_node::*;
+pub use self::child_distance_traverse::*;
 pub use self::distance_traverse::*;
 pub use self::iter::*;
